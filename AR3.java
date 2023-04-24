a78128420a031c3b0521472b0ad7b0383551c6d7
ShivaReddy23 <125042842+ShivaReddy23@users.noreply.github.com>
2023-04-24 22:05:47 +0530

    Add files via upload

---
+++ AR3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class AR3 {
+	  public static void main(String[] args) {
+	  List<String> list_Strings = new ArrayList<String>();
+	  list_Strings.add("Red");
+	  list_Strings.add("Green");
+	  list_Strings.add("Orange");
+	  list_Strings.add("White");
+	  list_Strings.add("Black");
+	  // Print the list
+	  System.out.println(list_Strings);
+	  list_Strings.add(0, "Pink");
+	  list_Strings.add(5, "Yellow");
+	  System.out.println(list_Strings);
+	 }
+}
