a867c99dc35d04fc64314d9180565c6e03314085
ShivaReddy23 <125042842+ShivaReddy23@users.noreply.github.com>
2023-04-25 16:50:22 +0530

    Add files via upload

---
+++ LL11.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+	import java.util.Iterator;
+public class LL11 {
+	  public static void main(String[] args) {
+	     LinkedList<String> l_list = new LinkedList<String>();
+	          l_list.add("Red");
+	          l_list.add("Green");
+	          l_list.add("Black");
+	          l_list.add("Pink");
+	          l_list.add("orange");
+	   System.out.println("Original linked list:" + l_list);  
+	  for(int p=0; p < l_list.size(); p++)
+	   {
+	      System.out.println("Element at index "+p+": "+l_list.get(p));
+	    } 
+	 }
+}
---
+++ LL23.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class LL23 {
+	 public static void main(String[] args) {
+	  LinkedList <String> linked_list = new LinkedList <String> ();
+	            linked_list.add("Red");
+	          linked_list.add("Green");
+	          linked_list.add("Black");
+	          linked_list.add("White");
+	          linked_list.add("Pink");
+	          System.out.println("Original linked list: " + linked_list);
+	     List<String> list = new ArrayList<String>(linked_list);
+
+	    for (String str : list){
+	      System.out.println(str);
+	    }
+	 }
+}
